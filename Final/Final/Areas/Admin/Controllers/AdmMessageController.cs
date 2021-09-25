52b0a8846d87ffa7ada35533dbb1ecd9842b1e31
SeferCeferli <60136439+SeferCeferli@users.noreply.github.com>
2021-09-25 18:36:03 +0400

    end

---
+++ Final/Final/Areas/Admin/Controllers/AdmMessageController.cs
@@ -0,0 +1,26 @@
+﻿using Final.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final.Areas.Admin.Controllers
+{
+    [Area("Admin"),Authorize]
+
+    public class AdmMessageController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AdmMessageController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Message.ToList());
+        }
+    }
+}
---
+++ Final/Final/Areas/Admin/Controllers/AdmSubscribeController.cs
@@ -0,0 +1,25 @@
+﻿using Final.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final.Areas.Admin.Controllers
+{
+    [Area("Admin"), Authorize]
+    public class AdmSubscribeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AdmSubscribeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Subscribes.ToList());
+        }
+    }
+}
